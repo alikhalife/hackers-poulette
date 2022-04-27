210ee137adce0777ecc7e6c557942e507e6e032e
Ali Khalife <alikhalife05@gmail.com>
2022-04-27 19:35:24 +0200

    Creating form. First touch styling form

--- index.php
+++ index.php
@@ -5,16 +5,49 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Contact Form</title>
+
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <title>Contact</title>
 </head>
 <body>
+<div>
+    <h2>Contact us </h2>
+</div>
 
-<form method="post" action="mail.php">
-    <label>Tape ton message ici pour m'écrire</label>
-    <textarea rows="10" cols="30" name="message"></textarea>
-    <input type="submit">
-</form>
+<div class="container">
+<h3>Want to reach out ? Just fill the form below, we will get back to you.</h3>
+    <form method="post" action=index.php>
+        <div class="row">
+            <div class ="col-25">
+                <label for="fname">Firstname</label>
+            </div>
+            <div class="col-75">
+                <input type="text" name=name" placeholder="John" required ><br>
+            </div>
+            <div class = "col-25">
+                <label for="lname">Lastname</label>
+            </div>
+            <div class="col-75">
+                <input type="text" id="lname" name="lastname" placeholder="Doe" required><br>
+            </div>
+            <div class = "col-25">
+                <label>Your email</label>
+            </div>
+            <div class="col-75">
+                <input type="email" name="email" placeholder="example@email.com" required><br>
+             </div>
+            <div class = "col-25">
+                <label>Message</label>
+            </div>
+         <div class="col-75">
+                <textarea name="message" placeholder="Type here..." required></textarea><br>
+            </div>
 
+            <input type="submit" value="Contact us">
+        </div>
+    </form>
+</div>
 </body>
 </html>
 <?php
